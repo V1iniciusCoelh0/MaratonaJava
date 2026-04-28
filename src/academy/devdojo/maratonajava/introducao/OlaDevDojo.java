07a642a897a3687a7006d4fcef805c7ba51367d7
V1niciusCoelh0 <viniciusmurce54@gmail.com>
2026-04-28 09:36:48 -0300

    comentarios

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -2,6 +2,11 @@ package academy.devdojo.maratonajava.introducao;
 
 public class OlaDevDojo {
     public static void main(String[] args) {
+        /*
+            comentario de varias
+            linhas
+         */
+        // comentario de uma unica linha
         System.out.println("KA-ME-HA-ME-HAAAAA");
     }
 }
