b4f84f57d0f3357ec46351adbcf29e49f8619e70
V1niciusCoelh0 <viniciusmurce54@gmail.com>
2026-04-28 10:15:51 -0300

    Exercicio com String e tipos primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+     static void main() {
+        String nome = "Vinicius";
+        String endereco = "2496 Rodovia Vinicius, Barra Velha, DF 67819-571";
+        double salario = 4500.90;
+        String dataDeRecebimento = "25/07";
+        System.out.println("Eu " +nome+ ", morando no endereço " + endereco + ", confirmo que recebi o salário de "+salario+ ", na data "+ dataDeRecebimento );
+    }
+}
